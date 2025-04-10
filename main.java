54ee1ac6a9b294eae4598de04d518048c0b54a54
Raul Quevedo <raul.quevedo@uas.edu.mx>
2025-04-09 20:28:36 -0700

    Primera version

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+       System.out.println("Hola mundo");
+
+        for (int i = 0; i < 10; i++) {
+            
+            System.out.println(i);
+  
+        }
+
+    }
+}
