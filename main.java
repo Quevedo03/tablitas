30bb111b2d5fd6b35ff060c412f5b18de9b13172
Raul Quevedo <raul.quevedo@uas.edu.mx>
2025-04-09 20:37:29 -0700

    Segunda version

--- main.java
+++ main.java
@@ -3,9 +3,11 @@ public class main {
     public static void main(String[] args) {
        System.out.println("Hola mundo");
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             
-            System.out.println(i);
+           for (int j = 1; j <= 10; j++) {
+                System.out.println(i+"X"+j+" = "+i*j);
+           }
   
         }
 
