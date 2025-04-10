9c419519a7781ce23f3d473f090cde5c1656ec0d
Raul Quevedo <raul.quevedo@uas.edu.mx>
2025-04-09 21:04:10 -0700

    Tercera version

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
        System.out.println("Hola mundo");
 
         for (int i = 1; i <= 10; i++) {
-            
+            System.out.println("\nTabla del: "+i);
            for (int j = 1; j <= 10; j++) {
                 System.out.println(i+"X"+j+" = "+i*j);
            }
